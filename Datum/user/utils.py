2ad87de0d7393e2c8a1fde8a23e739b75003a3c0
balyanin.mikhail <balyanin.mikhail@nppntt.ru>
2025-04-23 02:46:36 +0300

    Добавил отправку на email

--- Datum/user/utils.py
+++ Datum/user/utils.py
@@ -3,9 +3,18 @@ from django.contrib.auth.tokens import default_token_generator as token_generato
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import os
+
+SMTP_SERVER="smtp.mail.ru"
+SMTP_PORT=587
 
 def send_email_for_verify(request, user):
+    email = os.getenv('EMAIL_USER')
+    email_pass = os.getenv('EMAIL_PASS')
     current_site = get_current_site(request)
     context = {
         'user': user,
@@ -17,9 +26,17 @@ def send_email_for_verify(request, user):
         'registration/verify_email.html',
         context=context,
     )
-    email = EmailMessage(
-        'Verify email',
-        message,
-        to=[user.email],
-    )
-    email.send()
+
+    mail = MIMEMultipart('alternative')
+    mail['Subject'] = "Подтверждение учётной записи Datum"
+    mail['From'] = email.strip()
+    mail['To'] = ', '.join(str(user.email))
+    mail.attach(MIMEText(message, 'html'))
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(email, email_pass)
+        server.sendmail(email.strip(), str(user.email), message.as_string())
+
+    print(f"Письмо отправлено на: {str(user.email)}")
