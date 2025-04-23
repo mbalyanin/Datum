aca496486712ff00aa5046eae1516c3b72f43b41
balyanin.mikhail <balyanin.mikhail@nppntt.ru>
2025-04-23 22:56:06 +0300

    Пока вывод сообщения в консоль

--- Datum/user/utils.py
+++ Datum/user/utils.py
@@ -6,6 +6,7 @@ from django.template.loader import render_to_string
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from django.core.mail import EmailMessage
 from email.header import Header
 from email.utils import formatdate, make_msgid
 
@@ -15,8 +16,33 @@ SMTP_SERVER="smtp.mail.ru"
 SMTP_PORT=587
 
 def send_email_for_verify(request, user):
-    email_address = os.getenv('EMAIL_USER')
-    email_pass = os.getenv('EMAIL_PASS')
+    # email_address = os.getenv('EMAIL_USER')
+    # email_pass = os.getenv('EMAIL_PASS')
+    # current_site = get_current_site(request)
+    # context = {
+    #     'user': user,
+    #     'domain': current_site.domain,
+    #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+    #     'token': token_generator.make_token(user),
+    # }
+    # message = render_to_string(
+    #     'registration/verify_email.html',
+    #     context=context,
+    # )
+    #
+    # mail = MIMEMultipart('alternative')
+    # mail['Subject'] = "Подтверждение email Datum"
+    # mail['From'] = email_address.strip()
+    # mail['To'] = str(user.email).strip()
+    # mail.attach(MIMEText(message, 'html'))
+    #
+    # with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+    #     server.ehlo()
+    #     server.starttls()
+    #     server.login(email_address, email_pass)
+    #     server.sendmail(email_address.strip(), str(user.email).strip(), mail.as_string())
+    #
+    # print(f"Письмо отправлено на: {str(user.email)}")
     current_site = get_current_site(request)
     context = {
         'user': user,
@@ -28,17 +54,9 @@ def send_email_for_verify(request, user):
         'registration/verify_email.html',
         context=context,
     )
-
-    mail = MIMEMultipart('alternative')
-    mail['Subject'] = "Подтверждение email Datum"
-    mail['From'] = email_address.strip()
-    mail['To'] = str(user.email).strip()
-    mail.attach(MIMEText(message, 'html'))
-
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-        server.ehlo()
-        server.starttls()
-        server.login(email_address, email_pass)
-        server.sendmail(email_address.strip(), str(user.email).strip(), mail.as_string())
-
-    print(f"Письмо отправлено на: {str(user.email)}")
+    email = EmailMessage(
+        'Verify email',
+        message,
+        to=[user.email],
+    )
+    email.send()
